cded3308f004de790473613ce2716f8b7d7575e5
huangwang <212166829@qq.com>
2020-02-11 21:36:22 +0800

    更新中。。。

---
+++ leyou-search/src/main/java/com/leyou/LeyouSearchApplication.java
@@ -0,0 +1,11 @@
+package com.leyou;
+
+import com.netflix.discovery.EurekaNamespace;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableF
+public class LeyouSearchApplication {
+}
